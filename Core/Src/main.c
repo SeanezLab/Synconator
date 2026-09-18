f278d4876eae142588d55b1157c8f5ba14b1dfc2
rodolfokeesey <82533961+rodolfokeesey@users.noreply.github.com>
2026-09-18 18:27:27 -0500

    Had some issues with the lines getting stuck high so swapped to interrupt servicing again

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -119,7 +119,7 @@ int main(void)
   // starting timers
 
      HAL_TIM_Base_Start_IT(&htim6); // Communications loop (1000hz)
-//     HAL_TIM_Base_Start_IT(&htim7); // DMA Servicing loop (100khz)
+     HAL_TIM_Base_Start_IT(&htim7); // DMA Servicing loop (100khz)
 
 
 
@@ -144,7 +144,7 @@ int main(void)
 		  run_com_loop();
 	  }
 //	  HAL_GPIO_TogglePin(Timing_GPIO_Port, Timing_Pin);
-	  servicePulseDma(&stim_queue);
+//	  servicePulseDma(&stim_queue);
 //	  HAL_GPIO_TogglePin(Timing_GPIO_Port, Timing_Pin);
     /* USER CODE END WHILE */
 
